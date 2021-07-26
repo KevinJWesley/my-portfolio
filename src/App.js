74d1b953096dfff302c615b677ce413fa95cc1fd
kevinjwesley <kevin.john.wesley@gmail.com>
2021-07-26 16:07:00 -0600

    deploy test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import "./index.css";
@@ -7,7 +8,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <>
-      <Header />
       <Container />
       <Footer />
     </>
